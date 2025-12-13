c1ffc75cb9cfffecfbc3024fb3f619a5fe24707c
tayebdj <djatou.tayeb@gmail,com>
2025-12-13 04:02:52 +0100

    préparation pre prod

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,8 +1,5 @@
 export const environment = {
-    production: true,
-    // IMPORTANT: Update this with your actual Render backend URL after deployment
-    // Example: https://school-management-api.onrender.com
-    apiUrl: 'https://your-backend.onrender.com',
-    imagesPath: '/personne/'  // Path where images are stored
-  };
-  
+  production: true,
+  apiUrl: 'https://schoolmanager-api-iwzf.onrender.com',
+  imagesPath: '/personne/'
+};
